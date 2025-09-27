33546a9b97de144fc3d0db3f6c15071146f1160d
LiO2-coder <156443893+LiO2-coder@users.noreply.github.com>
2025-09-28 01:42:03 +0800

    Update __init__.py

--- MyFilter/__init__.py
+++ MyFilter/__init__.py
@@ -4,5 +4,5 @@
 # author_email="2099602919@qq.com",
 # description="this is a filter package include EWMAFilter, KalmanFilter, SWMFilter,
 # long_description="you can use this package to filter one dimensional data, such as mouse position, temperature, etc"
-# url="https://github.com/LiO2/MyFilter",
+# url="https://github.com/LiO2-coder/My_Filter",
 # how_to_use="from MyFilter import EWMAFilter.EWMAFilter"
